df4450fbe1cde974feac99f71cdc2446bd76498c
switzerrrland <switzerland.ptrs@gmail.com>
2021-05-10 01:06:08 +0300

    Added waiting for elements

--- src/test/java/ru/training/at/hw2/ex1/HeaderAndMenuTextTest.java
+++ src/test/java/ru/training/at/hw2/ex1/HeaderAndMenuTextTest.java
@@ -2,9 +2,10 @@ package ru.training.at.hw2.ex1;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-import static org.testng.Assert.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.List;
@@ -16,7 +17,6 @@ public class HeaderAndMenuTextTest {
         System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        //TODO: add waiting
         //1. Open test site by URL
         driver.navigate().to("https://jdi-testing.github.io/jdi-light/index.html");
         //2. Assert Browser title
@@ -25,22 +25,28 @@ public class HeaderAndMenuTextTest {
         //3. Perform login
         WebElement webElement;
         webElement = driver.findElement(By.cssSelector("a[href^=\"#\"]"));
+        WebDriverWait webDriverWait = new WebDriverWait(driver, 10);
+        webDriverWait.until(ExpectedConditions.visibilityOf(webElement));
         webElement.click();
         webElement = driver.findElement(By.id("name"));
+        webDriverWait.until(ExpectedConditions.visibilityOf(webElement));
         webElement.sendKeys("Roman");
         webElement = driver.findElement(By.id("password"));
+        webDriverWait.until(ExpectedConditions.visibilityOf(webElement));
         webElement.sendKeys("Jdi1234");
         webElement = driver.findElement(By.id("login-button"));
+        webDriverWait.until(ExpectedConditions.visibilityOf(webElement));
         webElement.click();
         //4. Assert Username is loggined
         webElement = driver.findElement(By.id("user-name"));
-        //TODO: change to data providers
         String actualUsername = webElement.getText();
-        softAssert.assertEquals(actualUsername, "ROMAN IOVLEV"); //TODO add visibility wait
+        webDriverWait.until(ExpectedConditions.visibilityOf(webElement));
+        softAssert.assertEquals(actualUsername, "ROMAN IOVLEV");
         //5. Assert that there are 4 items on the header section
-        // are displayed and they have proper texts TODO displayed
+        // are displayed and they have proper texts
         List<WebElement> headerElements = driver.findElements(By.cssSelector("ul."
                 + "uui-navigation.nav.navbar-nav.m-l8>li"));
+        webDriverWait.until(ExpectedConditions.visibilityOfAllElements(headerElements));
         softAssert.assertEquals(headerElements.size(), 4);
         softAssert.assertEquals(headerElements.get(0).getText(), "HOME");
         softAssert.assertEquals(headerElements.get(1).getText(), "CONTACT FORM");
@@ -48,42 +54,55 @@ public class HeaderAndMenuTextTest {
         softAssert.assertEquals(headerElements.get(3).getText(), "METALS & COLORS");
         //6. Assert that there are 4 images on the Index Page and they are displayed
         List<WebElement> benefitIcons = driver.findElements(By.cssSelector(".icons-benefit"));
+        webDriverWait.until(ExpectedConditions.visibilityOfAllElements(benefitIcons));
         softAssert.assertEquals(benefitIcons.size(), 4);
         softAssert.assertTrue(benefitIcons.get(0).isDisplayed());
         softAssert.assertTrue(benefitIcons.get(1).isDisplayed());
         softAssert.assertTrue(benefitIcons.get(2).isDisplayed());
         softAssert.assertTrue(benefitIcons.get(3).isDisplayed());
         //7. Assert that there are 4 texts on the Index Page under icons and they have proper text
-        //TODO create variables for actual text
         List<WebElement> benefitTexts = driver.findElements(By.cssSelector(".benefit-txt"));
+        webDriverWait.until(ExpectedConditions.visibilityOfAllElements(benefitTexts));
         softAssert.assertEquals(benefitTexts.size(), 4);
         softAssert.assertTrue(benefitTexts.get(0).isDisplayed());
-        softAssert.assertEquals(benefitTexts.get(0).getText(), "To include good practices\n"
+        String benefitText0 = "To include good practices\n"
                 + "and ideas from successful\n"
-                + "EPAM project");
+                + "EPAM project";
+        softAssert.assertEquals(benefitTexts.get(0).getText(), benefitText0);
         softAssert.assertTrue(benefitTexts.get(1).isDisplayed());
-        softAssert.assertEquals(benefitTexts.get(1).getText(), "To be flexible and\n"
-                + "customizable");
+        String benefitText1 = "To be flexible and\n"
+                + "customizable";
+        softAssert.assertEquals(benefitTexts.get(1).getText(), benefitText1);
         softAssert.assertTrue(benefitTexts.get(2).isDisplayed());
-        softAssert.assertEquals(benefitTexts.get(2).getText(), "To be multiplatform");
+        String benefitText2 = "To be multiplatform";
+        softAssert.assertEquals(benefitTexts.get(2).getText(), benefitText2);
         softAssert.assertTrue(benefitTexts.get(3).isDisplayed());
-        softAssert.assertEquals(benefitTexts.get(3).getText(), "Already have good base\n"
+        String benefitText3 = "Already have good base\n"
                 + "(about 20 internal and\n"
                 + "some external projects),\n"
-                + "wish to get more…");
+                + "wish to get more…";
+        softAssert.assertEquals(benefitTexts.get(3).getText(), benefitText3);
         //8. Assert that there is the iframe with “Frame Button” exist
-        driver.switchTo().frame("frame");
-        softAssert.assertTrue(driver.getPageSource().contains("frame")); //TODO not the same??
+        webElement = driver.findElement(By.id("frame"));
+        softAssert.assertTrue(webElement.isDisplayed());
         //9. Switch to the iframe and check that there is “Frame Button” in the iframe
+        driver.switchTo().frame("frame");
         webElement = driver.findElement(By.id("frame-button"));
+        webDriverWait.until(ExpectedConditions.visibilityOf(webElement));
         softAssert.assertTrue(webElement.isDisplayed());
         //10. Switch to original window back
-        driver.navigate().to("https://jdi-testing.github.io/jdi-light/index.html");
+        driver.switchTo().defaultContent();
         //11. Assert that there are 5 items in the Left Section
         // are displayed and they have proper text
         List<WebElement> leftSectionElements = driver.findElements(By.cssSelector(
                 "ul.sidebar-menu.left>li"));
+        webDriverWait.until(ExpectedConditions.visibilityOfAllElements(leftSectionElements));
         softAssert.assertEquals(leftSectionElements.size(), 5);
+        softAssert.assertEquals(leftSectionElements.get(0).getText(), "Home");
+        softAssert.assertEquals(leftSectionElements.get(1).getText(), "Contact form");
+        softAssert.assertEquals(leftSectionElements.get(2).getText(), "Service");
+        softAssert.assertEquals(leftSectionElements.get(3).getText(), "Metals & Colors");
+        softAssert.assertEquals(leftSectionElements.get(4).getText(), "Elements packs");
         //12. Close Browser
         driver.close();
     }
--- src/test/java/ru/training/at/hw2/ex2/CheckboxTest.java
+++ src/test/java/ru/training/at/hw2/ex2/CheckboxTest.java
@@ -1,12 +1,14 @@
 package ru.training.at.hw2.ex2;
 
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.asserts.SoftAssert;
 import java.util.List;
 
 public class CheckboxTest {
@@ -16,49 +18,59 @@ public class CheckboxTest {
         System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        //TODO: add waiting
         //1. Open test site by URL
         driver.navigate().to("https://jdi-testing.github.io/jdi-light/index.html");
         //2. Assert Browser title
-        assertEquals(driver.getTitle(), "Home Page");
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(driver.getTitle(), "Home Page");
         //3. Perform login
         WebElement webElement;
         webElement = driver.findElement(By.cssSelector("a[href^=\"#\"]"));
+        WebDriverWait webDriverWait = new WebDriverWait(driver, 10);
+        webDriverWait.until(ExpectedConditions.visibilityOf(webElement));
         webElement.click();
         webElement = driver.findElement(By.id("name"));
+        webDriverWait.until(ExpectedConditions.visibilityOf(webElement));
         webElement.sendKeys("Roman");
         webElement = driver.findElement(By.id("password"));
+        webDriverWait.until(ExpectedConditions.visibilityOf(webElement));
         webElement.sendKeys("Jdi1234");
         webElement = driver.findElement(By.id("login-button"));
+        webDriverWait.until(ExpectedConditions.visibilityOf(webElement));
         webElement.click();
         //4. Assert Username is loggined
         webElement = driver.findElement(By.id("user-name"));
-        //TODO: change to data providers
         String actualUsername = webElement.getText();
-        assertEquals(actualUsername, "ROMAN IOVLEV"); //TODO add visibility wait
+        webDriverWait.until(ExpectedConditions.visibilityOf(webElement));
+        softAssert.assertEquals(actualUsername, "ROMAN IOVLEV");
         //5. Open through the header menu Service -> Different Elements Page
         List<WebElement> headerElements = driver.findElements(By.cssSelector("ul."
                 + "uui-navigation.nav.navbar-nav.m-l8>li"));
+        webDriverWait.until(ExpectedConditions.visibilityOfAllElements(headerElements));
         webElement = headerElements.get(2);
         webElement.click();
         webElement = driver.findElement(By.cssSelector("a[href*=\"different-elements.html\"]"));
+        webDriverWait.until(ExpectedConditions.visibilityOf(webElement));
         webElement.click();
         //6. Select checkboxes Water, Wind
         List<WebElement> checkboxes = driver.findElements(
                 By.cssSelector("input[type*=\"checkbox\"]"));
+        webDriverWait.until(ExpectedConditions.visibilityOfAllElements(checkboxes));
         webElement = checkboxes.get(0);
         webElement.click();
         webElement = checkboxes.get(2);
         webElement.click();
         //7. Select radio Selen
         List<WebElement> radioButtons = driver.findElements(By.name("metal"));
+        webDriverWait.until(ExpectedConditions.visibilityOfAllElements(radioButtons));
         webElement = radioButtons.get(3);
         webElement.click();
         //8. Select in dropdown Yellow
-        //webElement = driver.findElement(By.className("uui-form-element"));
         webElement = driver.findElement(By.cssSelector("select"));
+        webDriverWait.until(ExpectedConditions.visibilityOf(webElement));
         webElement.click();
         List<WebElement> options = driver.findElements(By.cssSelector("option"));
+        webDriverWait.until(ExpectedConditions.visibilityOfAllElements(options));
         webElement = options.get(3);
         webElement.click();
         //9. Assert that for each checkbox there is an individual log row
@@ -68,6 +80,7 @@ public class CheckboxTest {
         //for dropdown there is a log row and value is corresponded to the selected value.
         List<WebElement> logEntries = driver.findElements(
                 By.cssSelector("ul.panel-body-list.logs>li"));
+        webDriverWait.until(ExpectedConditions.visibilityOfAllElements(logEntries));
         String waterLog = logEntries.get(3).getText();
         assertTrue(waterLog.contains("Water: condition changed to true"));
         String windLog = logEntries.get(2).getText();
